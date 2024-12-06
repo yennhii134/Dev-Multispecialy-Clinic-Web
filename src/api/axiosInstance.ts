b8b67aa5c62e7c0f391e7dc6a67676e55c654abd
BuiTriThuc1506 <20088361.thuc@student.iuh.edu.vn>
2024-12-06 13:52:29 +0700

    add domain deploy

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const DOMAIN = "http://localhost:8000/";
+const DOMAIN = "https://mnq7qzk3-8000.asse.devtunnels.ms/";
 
 export const axiosInstance = axios.create({
   baseURL: DOMAIN,
